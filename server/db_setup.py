cc1204dbb346828cea975da1ccf1b598507cf696
chandhru_27 <chandhru.l2007@gmail.com>
2025-11-05 20:47:09 +0530

    enhanced db setup with connection pooling

--- server/db_setup.py
+++ server/db_setup.py
@@ -1,39 +1,52 @@
 import psycopg2
+from psycopg2 import pool
 import os
 from dotenv import load_dotenv
 from schema import SCHEMA_LIST
-# Load environment variables
+from contextlib import contextmanager
+
 load_dotenv()
 
-def initialize_database_and_create_tables():
-    conn = None
-    cur = None
+postgreSQL_pool = None
+
+def initialize_connection_pool():
+    """Create connection pool in a global pool variable"""
+    global postgreSQL_pool
     try:
-        # Connect to PostgreSQL
-        conn = psycopg2.connect(
+        postgreSQL_pool = pool.ThreadedConnectionPool(
+            minconn=1,
+            maxconn=20,
             host=os.getenv('PG_HOST'),
             database=os.getenv('PG_DB'),
             user=os.getenv('PG_USER'),
             password=os.getenv('PG_PASSWORD'),
             port=os.getenv('PG_PORT'),
-            sslmode='disable'
+            sslmode='disable' 
         )
-
-        cur = conn.cursor()
-
-        for create_table in SCHEMA_LIST:
-            cur.execute(create_table)
-
-        # Commit the changes
-        conn.commit()
-
-        print("All tables created successfully or already exist.")
-
+        print("Connection pool created succesuccessfully")
     except psycopg2.Error as e:
-        print("Database error:", e)
+        print(f"Error initializing connection pool: {e}")
 
+@contextmanager
+def get_db_connection():
+    """Context manager for database connections"""
+    conn = postgreSQL_pool.getconn()
+    try:
+        yield conn
     finally:
-        if cur:
+        postgreSQL_pool.putconn(conn=conn)
+
+def initialize_database_and_create_tables():
+    """Create all tables on startup"""
+    with get_db_connection() as conn:
+        cur = conn.cursor()
+        try:
+            for create_table in SCHEMA_LIST:
+                cur.execute(create_table)
+            conn.commit()
+            print("All tables created successfully or already exists")
+        except psycopg2.Error as e:
+            print(f"Database error: {e}")
+            conn.rollback()
+        finally:
             cur.close()
-        if conn:
-            conn.close()
