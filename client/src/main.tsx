b144c3bbec014e11f6c803ec12034ccdc4289f9e
Chandhru-27 <chandhru.l2007@gmail.com>
2025-11-16 22:07:25 +0530

    tanstack-query global configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,25 @@
 import { createRoot } from "react-dom/client";
+import React from "react";
 import App from "./App";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+      retry: 1,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+);
