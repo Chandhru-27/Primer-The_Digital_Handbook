8c5646ff487b7df99002425a62b80dec6662c20e
Chandhru-27 <chandhru.l2007@gmail.com>
2025-11-18 13:12:08 +0530

    feat : add auth context provider wrapper to app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AuthProvider } from "./lib/auth/auth-context";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,8 +19,10 @@ const queryClient = new QueryClient({
 });
 
 createRoot(document.getElementById("root")!).render(
-    <QueryClientProvider client={queryClient}>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </AuthProvider>
+  </QueryClientProvider>
 );
